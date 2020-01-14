d5f9bfec68783c833e56f621f6e21e5d407c99ed
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-01-14 13:55:31 +0100

    add tooltip

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,3 +5,23 @@ document.querySelector('.navbar-toggler').addEventListener('click', () => {
 // resize mdEditor and content
 const resizeBarElement = document.querySelector('.resize-bar');
 resizeDivs(resizeBarElement, "H");
+
+// create tooltip
+const btnToolTip = document.querySelectorAll('[data-tooltip]');
+btnToolTip.forEach(t => {
+  t.addEventListener("mouseover", (e) => {
+    if (e.offsetX >= 0 && e.offsetX <= t.offsetWidth && e.offsetY >= 0 && e.offsetY <= t.offsetHeight) {     
+      var tooltip = document.createElement("span");
+
+      tooltip.classList.add('tooltiptext');
+      tooltip.style.top = (t.offsetTop+30) + 'px';
+      tooltip.style.left = (t.offsetLeft+10) + 'px';
+      tooltip.textContent = t.getAttribute('data-tooltip');
+      document.body.appendChild(tooltip)
+    }
+  });
+
+  t.addEventListener("mouseout", () => {
+    document.body.removeChild(document.querySelector('.tooltiptext'))
+  });
+});
