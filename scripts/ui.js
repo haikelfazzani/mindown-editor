f020231b6d0305e12cdf5202ecc75ab2608d17f1
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-01-14 14:07:11 +0100

    add tooltip

--- scripts/ui.js
+++ scripts/ui.js
@@ -9,19 +9,24 @@ resizeDivs(resizeBarElement, "H");
 // create tooltip
 const btnToolTip = document.querySelectorAll('[data-tooltip]');
 btnToolTip.forEach(t => {
-  t.addEventListener("mouseover", (e) => {
+  t.addEventListener("mouseover", (e) => {  
     if (e.offsetX >= 0 && e.offsetX <= t.offsetWidth && e.offsetY >= 0 && e.offsetY <= t.offsetHeight) {     
       var tooltip = document.createElement("span");
+      tooltip.classList.add('tooltiptext');      
 
-      tooltip.classList.add('tooltiptext');
       tooltip.style.top = (t.offsetTop+30) + 'px';
       tooltip.style.left = (t.offsetLeft+10) + 'px';
+
       tooltip.textContent = t.getAttribute('data-tooltip');
-      document.body.appendChild(tooltip)
+      document.body.appendChild(tooltip);      
+
+      if((tooltip.offsetWidth+tooltip.offsetLeft) >= screen.width) {
+        tooltip.style.left = (screen.width-(tooltip.offsetWidth+50)) + 'px';
+      }      
     }
   });
 
   t.addEventListener("mouseout", () => {
-    document.body.removeChild(document.querySelector('.tooltiptext'))
+    document.body.removeChild(document.querySelector('.tooltiptext'));
   });
 });
