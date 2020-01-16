a3aad30c6fffc4e807949a7eb56adf8c3e74096e
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-01-16 20:59:16 +0100

    feat:pwa workbox

--- sw.js
+++ sw.js
@@ -4,51 +4,30 @@ if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
   workbox.precaching.precacheAndRoute([{ url: '/index.html', revision: 'abcd1234' },]);
-  
+
   workbox.routing.registerRoute(
     /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    })
+    new workbox.strategies.CacheFirst({ cacheName: 'images' })
   );
 
   workbox.routing.registerRoute(
-    /\.(?:js|css|html)$/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'static-resources',
-    })
+    /\.(?:css)$/,
+    new workbox.strategies.CacheFirst({ cacheName: 'css-resources' })
   );
 
-
-  // Cache the underlying font files with a cache-first strategy for 1 year.
-  // https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css
   workbox.routing.registerRoute(
-    /^https:\/\/stackpath.bootstrapcdn.com\/bootstrap\/4.4.1\/css\/bootstrap.min.css/,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'bootstrap',
-      plugins: [
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200],
-        }),
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-          maxEntries: 30,
-        }),
-      ],
-    })
+    new RegExp('/scripts/'),
+    new workbox.strategies.CacheFirst({ cacheName: 'js-resources' })
   );
 
-  // Cache the underlying font files with a cache-first strategy for 1 year.
-  // https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css
+  workbox.routing.registerRoute(
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
+    new workbox.strategies.NetworkFirst({ cacheName: 'bootstrap' }),
+  );
+
   workbox.routing.registerRoute(
     'https://use.fontawesome.com/releases/v5.5.0/css/all.css',
-    new workbox.strategies.StaleWhileRevalidate({ cacheName: 'fontawesome' }),
+    new workbox.strategies.NetworkFirst({ cacheName: 'fontawesome' }),
   );
 
 } else {
